be28580c7429e67ddb672ac6bbfe75d2012f4e7b
Belval <edouard@belval.org>
2018-11-08 15:58:32 -0500

    Add Azure

--- README.md
+++ README.md
@@ -10,10 +10,26 @@ The interesting piece is not how such thing works, but how it can be deployed on
 Here's what each script does:
 
 - `app.py`: The application
-- `amazon-web-service.sh`: Deploy to AWS
-- `azure.sh`: Deploy to Azure
-- `google-cloud.sh`: Deploy to Google Cloud
+- `microsoft/azure.sh`: Deploy to Azure
+- `google/google_cloud.sh`: Deploy to Google Cloud
+- `amazon/aws.sh`: Deploy to AWS (Coming soon!)
+
+## Valuable information
+
+- The user that will be used to run this script must be part of the docker group other wise it will not work.
+- When deployed on Google Cloud, the app is on port 80. When deployed to Azure it is on port 5000 because they do not offer forwarding.
 
 ## Why is this interesting?
 
 It shows the differences between all three providers
+
+## My personal opinion regarding the providers
+
+Disclaimer: My experience with these services is shallow and the opinions you can read here are not to be taken as absolute. 
+
+As of 2018, and after deploying on Google Cloud, Microsoft Azure, Amazon AWS:
+
+- For all three I resorted to third party tutorials instead of the official documentation because I found it be lacking.
+- Azure as, by far, the best web interface. It is clean, easy to use and there is a feeling of overall cohesion.
+- Using Kubernetes on Google Cloud felt more natural than on the other two.
+- Azure does not offer port forwarding. For example, I am serving my application on port 5000 on the container, but the instance cannot forward its traffic from 80 to 5000 easily. This is available with Google Cloud.
--- microsoft/azure.sh
+++ microsoft/azure.sh
@@ -1,41 +1,46 @@
-# Install azure-cli
-curl -L https://aka.ms/InstallAzureCli | bash
-
-# Re-sourcing our bashrc
-exec -l $SHELL
+# Piping to to bash is always a bad idea. Feel free
+# to install the Azure CLI SDK yourself and comment that line.
+#curl -L https://aka.ms/InstallAzureCli | bash
 
 # Login
 az login
 
-# Create a resource group
-az group create --name pdf2image-demo-resource --location eastus
+# Creating the resource group
+az group create --name pdf2imageDemoGroup --location eastus
 
-# Create an Azure container registry
-az acr create --resource-group pdf2image-demo-resource --name pdf2image-demo --sku Basic
+# Creating the container registry
+az acr create --resource-group pdf2imageDemoGroup --name pdf2imageDemo --sku Basic
 
-# Log in Azure container registry
-az acr login --name pdf2image-demo
+# Log into the container registry
+az acr login --name pdf2imageDemo
 
-# Build docker container
-docker build ../app -t pdf2image-demo
+# Build docker image
+docker build ../app -t pdf2image-demo-app
 
-# Tag the image in the azure image registry
-TAG=$(az acr show --name pdf2image-demo --query loginServer)
+# Tag image in Azure image registry
+TAG=$(az acr show --name pdf2imageDemo --query loginServer)
+TAG=${TAG//\"}
 
-# Tag docker image
-docker tag pdf2image-demo $TAG/pdf2image-demo:v1
+# Tag the docker image
+docker tag pdf2image-demo-app $TAG/pdf2image-demo-app:v1
 
-# Push the image to the registry
-docker push $TAG/pdf2image-demo:v1
+# Push the docker image to the registry
+docker push $TAG/pdf2image-demo-app:v1
 
-# Enable admin permission on the registry to deploy
-az acr update -n pdf2image-demo --admin-enabled true
+# Enable administrative rights
+az acr update -n pdf2imageDemo --admin-enabled true
 
-# Get password for deployment
-SECRET=$(az acr credential show --name pdf2image-demo --query "passwords[0].value")
+# Get the deployment password
+PASSWORD=$(az acr credential show --name pdf2imageDemo --query "passwords[0].value")
+PASSWORD=${PASSWORD//\"}
 
-echo "Your password is: "
-echo $SECRET
+# Finally, deploy the docker container
+# When prompted, enter pdf2imageDemo
+az container create --resource-group pdf2imageDemoGroup --name pdf2image-demo-app --image $TAG/pdf2image-demo-app:v1 --cpu 1 --memory 1 --ip-address public --ports 5000 --registry-password "$PASSWORD"
 
-az container create --resource-group pdf2image-demo-resource --name pdf2image-demo --image $TAG/pdf2image-demo:v1 --cpu 1 --memory 1 --ip-address public --ports 80 --registry-password "$SECRET"
+
+# Get the IP address of the container
+az container show --resource-group pdf2imageDemoGroup --name pdf2image-demo-app --query ipAddress.ip
+
+
 
